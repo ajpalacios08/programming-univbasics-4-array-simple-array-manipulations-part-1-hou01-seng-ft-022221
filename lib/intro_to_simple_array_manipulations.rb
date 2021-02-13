ae2ec41cad10f1a2d95ba0ebeaa0ecd9521fad05
Allen Palacios <apalacios1994@gmail.com>
2021-02-13 20:46:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,7 @@ def using_pop(continents)
  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   @continents.pop
 end
+
+def pop_with_args(@dog_breeds)
+   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+   @dog_breeds.pop
